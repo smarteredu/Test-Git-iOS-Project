f648c5e92057929b151e18fa205e6860edb3f548
Jalal Bajrai <j.bajrai@gmail.com>
2014-04-30 23:39:02 +0300

    added a test class

---
+++ Git Test/SETest.h
@@ -0,0 +1,13 @@
+//
+//  SETest.h
+//  Git Test
+//
+//  Created by JB on 4/30/14.
+//  Copyright (c) 2014 Smarteredu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SETest : NSObject
+
+@end
---
+++ Git Test/SETest.m
@@ -0,0 +1,13 @@
+//
+//  SETest.m
+//  Git Test
+//
+//  Created by JB on 4/30/14.
+//  Copyright (c) 2014 Smarteredu. All rights reserved.
+//
+
+#import "SETest.h"
+
+@implementation SETest
+
+@end
